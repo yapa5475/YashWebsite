fad743cd64958238a4dd335f430927d145f3d84c
yapa5475 <yash.parekh223@gmail.com>
2018-01-16 23:43:49 -0700

    Trying to connect to database/php - try 1

--- mailyash.php
+++ mailyash.php
@@ -1,13 +1,28 @@
 <?php 
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$message = $_POST['message'];
+	$con = "dbname=ddp4aldlbkqo6j host=ec2-54-235-73-241.compute-1.amazonaws.com
+ user=ohtzgwtgrpmoiv password=559436baeb4efdf843f45b1c2dff7cdf1f764d6910cb62cb35d7cdc836595e50 sslmode=require";
 
-$formcontent = "From: $firstname $lastname \n Message: $message";
-$recipient = "yash.parekh223@gmail.com";
-$subject = "Yash Website: $firstname wants to contact you!";
-$mailheader = "From: $email \r\n";
+ 	if(!$con){
+ 		echo "Database connection failed";
+ 	}
+ 	else {
+ 		echo "Database connection success";
+ 	}
 
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error");
-echo "Thank you";
+	$firstname = $_POST['firstname'];
+	$lastname = $_POST['lastname'];
+	$message = $_POST['message'];
+/*
+	echo $firstname;
+	echo $lastname;
+	echo $message;
+
+	$formcontent = "From: $firstname $lastname \n Message: $message";
+	$recipient = "yash.parekh223@gmail.com";
+	$subject = "Yash Website: $firstname wants to contact you!";
+	$mailheader = "From: $email \r\n";
+
+	mail($recipient, $subject, $formcontent, $mailheader) or die("Error");
+	echo "Thank you";
+	*/
 ?>
